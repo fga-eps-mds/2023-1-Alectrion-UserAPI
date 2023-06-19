c86a0fb5dfe7a2218e1c93e92cc3e6192e1c9b7d
esteerlino <esteerlino@gmail.com>
2023-06-18 21:10:03 -0300

    #126 feat: adiciona cpf nas migracoes
    Co-authored-by: PabloGJBS <plabo.jbs@gmail.com>

--- src/db/migrations/1664151733139-create-users.ts
+++ src/db/migrations/1664151733139-create-users.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 export class createUsers1664151733139 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      "INSERT INTO \"user\"(name, email, job, role, username, password) VALUES('admin', 'admin@admin.com', 'generico', 'administrador', 'admin', '$2a$04$cZtiKglD8y45.1U8QAGhOOPxggFzE4WwDl5Er.fs5XmAVASWysdtm')"
+      "INSERT INTO \"user\"(name, cpf, email, job, role, username, password) VALUES('admin', '12345678910', 'admin@admin.com', 'generico', 'administrador', 'admin', '$2a$04$cZtiKglD8y45.1U8QAGhOOPxggFzE4WwDl5Er.fs5XmAVASWysdtm')"
     )
   }
 
